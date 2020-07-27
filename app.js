609d6615675634cb051698594033ee24d30bd553
José Nery <josenerydev@gmail.com>
2020-07-27 14:41:21 -0300

    27. Functions as Types

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ function add(n1, n2) {
 }
 function printResult(num) {
     console.log('Result: ' + num);
-    return;
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
 // let someValue: undefined;
