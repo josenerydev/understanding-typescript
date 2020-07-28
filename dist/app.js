a96d843cdb6347af844f263feb2e3752f5d2a738
José Nery <josenerydev@gmail.com>
2020-07-27 21:15:40 -0300

    55. How Code Gets Compiled

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,17 @@
 // const userName = 'Max';
 // userName = 'Maximilian';
 // let age = 30;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // age = 29;
 // function add(a: number, b: number) {
 //   let result;
@@ -19,22 +30,26 @@
 //   button.addEventListener('click', event => console.log(event))
 // }
 // printOutput(add(5));
-const hobbies = ['Sports', 'Cooking', 'Hiking'];
-const activeHobbies = ['Hiking'];
-activeHobbies.push(...hobbies);
-const person = {
+var hobbies = ['Sports', 'Cooking', 'Hiking'];
+var activeHobbies = ['Hiking'];
+activeHobbies.push.apply(activeHobbies, hobbies);
+var person = {
     firstName: 'Max',
     age: 30
 };
-const copiedPerson = Object.assign({}, person);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
+var copiedPerson = __assign({}, person);
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (curResult, curValue) {
         return curResult + curValue;
     }, 0);
 };
-const addedNumbers = add(5, 10, 2, 3.7);
+var addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
 console.log(hobby1, hobby2, remainingHobbies);
-const { firstName: userName, age } = person;
+var userName = person.firstName, age = person.age;
 console.log(userName, age, person);
