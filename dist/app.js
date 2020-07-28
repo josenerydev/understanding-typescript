8aaebd3b990e8ac8598fdb2d0ca5b894239c9e0c
José Nery <josenerydev@gmail.com>
2020-07-28 14:00:52 -0300

    87. Index Properties

--- dist/app.js
+++ dist/app.js
@@ -61,3 +61,7 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
     userInputElement.value = 'Hi there!';
 }
+const errorBag = {
+    email: 'Not a valid email!',
+    username: 'Must start with a capital character!'
+};
