7d873b7dce27eccb3aaa66dc97971e14a0dbfcf6
José Nery <josenerydev@gmail.com>
2020-07-28 15:04:56 -0300

    95. Creating a Generic Function

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,16 @@
 "use strict";
-const names = [];
-// names[0].split(' ');
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(10);
-    }, 2000);
-});
-promise.then(data => {
-    // data.split(' ');
-});
+// const names: Array<string> = [];
+// // names[0].split(' ');
+// const promise = new Promise<number>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+// promise.then(data => {
+//   // data.split(' ');
+// })
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.age);
