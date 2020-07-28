061162a8e18c96a422e04d4e6ba5ea8c9520aa49
José Nery <josenerydev@gmail.com>
2020-07-28 11:53:55 -0300

    78. Optional Parameters & Properties

--- dist/app.js
+++ dist/app.js
@@ -7,15 +7,22 @@ add = function (n1, n2) {
 var Person = /** @class */ (function () {
     function Person(n) {
         this.age = 30;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     Person.prototype.greet = function (phrase) {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
     };
     return Person;
 }());
 var user1;
-user1 = new Person('Max');
+user1 = new Person();
 // user1.name = 'Manu';
 user1.greet(' Hi there - I am');
 console.log(user1);
