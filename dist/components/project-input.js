c42c7b94523ed4499f5921ca7ed1fe2ea8c30195
José Nery <josenerydev@gmail.com>
2020-07-29 19:44:30 -0300

    147. Understanding various Import & Export Syntaxes

--- dist/components/project-input.js
+++ dist/components/project-input.js
@@ -4,9 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { Component } from './base-component.js';
-import { validate } from '../utils/validation.js';
-import { AutoBind } from '../decorators/autobind.js';
+import Component from './base-component.js';
+import * as Validation from '../utils/validation.js';
+import { AutoBind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 // ProjectInput Class
 export class ProjectInput extends Component {
@@ -40,9 +40,9 @@ export class ProjectInput extends Component {
             min: 1,
             max: 5
         };
-        if (!validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(peopleValidatable)) {
+        if (!Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peopleValidatable)) {
             alert('Invalid input, please try again!');
             return;
         }
@@ -66,5 +66,5 @@ export class ProjectInput extends Component {
     }
 }
 __decorate([
-    AutoBind
+    Autobind
 ], ProjectInput.prototype, "submitHandler", null);
--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,6 +1,6 @@
-import { Component } from './base-component.js';
-import { Validatable, validate } from '../utils/validation.js';
-import { AutoBind } from '../decorators/autobind.js';
+import Component from './base-component.js';
+import * as Validation from '../utils/validation.js';
+import { AutoBind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 
 // ProjectInput Class
@@ -28,16 +28,16 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5
     };
-    const peopleValidatable: Validatable = {
+    const peopleValidatable: Validation.Validatable = {
       value: +enteredPeople,
       required: true,
       min: 1,
@@ -45,9 +45,9 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     };
 
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(peopleValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(peopleValidatable)
     ) {
       alert('Invalid input, please try again!');
       return;
@@ -62,7 +62,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.peopleInputElement.value = '';
   }
 
-  @AutoBind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.getherUserInput();
