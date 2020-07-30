b60a5cdf42ebd1b4e9bd516fb82629b884e85bef
José Nery <josenerydev@gmail.com>
2020-07-30 13:04:17 -0300

    172. Rendering a Map with Google Maps incl.Types

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ const addressInput = document.getElementById('address') as HTMLInputElement;
 
 const GOOGLE_API_KEY = '';
 
+// declare var google: any;
+
 type GoogleGeocodingResponse = {
   results: {
     geometry: {
@@ -30,6 +32,12 @@ function searchAddressHandler(event: Event) {
       throw new Error('Could not fetch location!');
     }
     const coordinates = response.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById("map")!, {
+      center: coordinates,
+      zoom: 16
+    });
+
+    new google.maps.Marker({ position: coordinates, map: map });
   }).catch(err => {
     alert(err.message);
     console.log(err);
